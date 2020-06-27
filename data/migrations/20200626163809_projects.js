308a35244413942e4ac7bedc15737963e81fcd1a
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-26 17:18:09 -0700

    built out clean-seed

--- data/migrations/20200626163809_projects.js
+++ data/migrations/20200626163809_projects.js
@@ -1,8 +1,8 @@
+exports.up = function (knex) {};
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("project_resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
 };
