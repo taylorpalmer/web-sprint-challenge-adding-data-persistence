01d835ca210e4933ab83299c5726b4e158c7401e
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-26 18:15:20 -0700

    added project_resource seed file, running router tests now

--- data/seeds/01-project.js
+++ data/seeds/01-project.js
@@ -1,6 +1,10 @@
 exports.seed = async function (knex) {
   await knex("projects").insert([
-    { name: "build table", completed: false },
+    {
+      name: "build table",
+      description: "We're tired of eating on the floor",
+      completed: false,
+    },
     { name: "pick nose", completed: true },
   ]);
 };
--- data/seeds/03-task.js
+++ data/seeds/03-task.js
@@ -1,8 +1,14 @@
 exports.seed = async function (knex) {
   await knex("tasks").insert([
-    { description: "join legs and back to seat piece with nails and hammer" },
     {
+      project_id: 1,
+      description: "join legs to flat part with nails and hammer",
+      completed: false,
+    },
+    {
+      project_id: 2,
       description: "stick finger in nose and look for goodies",
+      notes: "duh",
       completed: true,
     },
   ]);
