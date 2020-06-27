6a24ef498a41812565c594700c1a723ed8ce9b4c
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-26 17:29:59 -0700

    built migration

--- data/migrations/20200626163809_projects.js
+++ data/migrations/20200626163809_projects.js
@@ -1,4 +1,45 @@
-exports.up = function (knex) {};
+exports.up = async function (knex) {
+  await knex.schema.createTable("projects", (table) => {
+    table.increments("id");
+    table.text("name").notNull().unique();
+    table.text("description");
+    table.boolean("completed").defaultTo(false).notNull();
+  });
+
+  await knex.schema.createTable("resources", (table) => {
+    table.increments("id");
+    table.text("name").notNull().unique();
+    table.text("description");
+  });
+
+  await knex.schema.createTable("tasks", (table) => {
+    table.increments("id");
+    table
+      .integer("project_id")
+      .references("id")
+      .inTable("projects")
+      .onDelete("SET NULL")
+      .onUpdate("CASCADE");
+    table.text("description").notNull().unique();
+    table.text("notes");
+    table.boolean("completed").defaultTo(false).notNull();
+  });
+
+  await knex.schema.createTable("project_resources", (table) => {
+    table
+      .integer("project_id")
+      .references("id")
+      .inTable("projects")
+      .onDelete("SET NULL")
+      .onUpdate("CASCADE");
+    table
+      .integer("resource_id")
+      .references("id")
+      .inTable("resources")
+      .onDelete("SET NULL")
+      .onUpdate("CASCADE");
+  });
+};
 
 exports.down = async function (knex) {
   await knex.schema.dropTableIfExists("project_resources");
