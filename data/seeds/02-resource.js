64cae8dffb62afe976c1dc72b9e34a07a22a5520
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-26 17:55:35 -0700

    built seeds

--- data/seeds/02-resource.js
+++ data/seeds/02-resource.js
@@ -1,13 +1,9 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex("resources").insert([
+    { name: "wood", description: "sturdy" },
+    { name: "hammer" },
+    { name: "nails", description: "straight, sharp" },
+    { name: "nose", description: "stuffed" },
+    { name: "finger", description: "nimble" },
+  ]);
 };
--- data/seeds/03-task.js
+++ data/seeds/03-task.js
@@ -1,13 +1,9 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex("tasks").insert([
+    { description: "join legs and back to seat piece with nails and hammer" },
+    {
+      description: "stick finger in nose and look for goodies",
+      completed: true,
+    },
+  ]);
 };
